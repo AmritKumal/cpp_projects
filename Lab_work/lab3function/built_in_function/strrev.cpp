39762789c779c0af099187b49d37d7af6e1a204b
Amrit Kumal <amritkumal93@gmail.com>
2021-06-05 09:56:04 +0545

    strrev()

---
+++ Lab_work/lab3function/built_in_function/strrev.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char string[100];
+    cout<<"Input your string:"<<endl;
+    cin>>string;
+    cout<<"string reversed is:"<<strrev(string)<<endl;
+}
